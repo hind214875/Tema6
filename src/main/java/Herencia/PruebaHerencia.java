399ec57aa651b0d0ce74de551917bad0d2454a44
Hind <hindsamiri2@gmail.com>
2022-03-10 11:54:43 +0100

    añadir classe prueba

--- src/main/java/Herencia/PruebaHerencia.java
+++ src/main/java/Herencia/PruebaHerencia.java
@@ -17,6 +17,14 @@ public class PruebaHerencia {
         Turismo t1 = new Turismo(); // Constructor por defecto de Java
         atributos = t1.toString();
         System.out.println(atributos);
+
+        // Creación de instancias de la superclase Vehiculo y
+        // de las subclases Turismo, Deportivo y Furgoneta
+        Vehiculo miVehiculo = new Vehiculo("4050 ABJ", "VW", "GTI", "", 100.0, true);
+        Turismo miTurismo = new Turismo("345674", "4060 TUR", "Skoda", "Fabia", 90.00, false, 2, true);
+        Deportivo miDeportivo = new Deportivo(2, "HG4568", "Mercedes", "wv", "blue", 100.00, true);
+        Furgoneta miFurgoneta = new Furgoneta(10, 20,"JK15489", "furgoneta", "dgd" ,"verde", 500, false);
+
     }
 
 }
