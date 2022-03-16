8fb01a45daa4ada12152ad0f7ab6ccc0e3447223
Hind <hindsamiri2@gmail.com>
2022-03-16 14:26:55 +0100

    añadir nuevos ejercicios

---
+++ src/main/java/ejercicio4511/Ej4.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio4511;
+
+/**
+ *
+ * @author hinda
+ */
+public class Ej4 {
+    /*4.- ¿Qué diferencia existe entre sobrecargar un método y sobrescribir un método?*/
+ /*Sobrecargar significa definir nuevos métodos. 
+ Sobrescribir significa ocultar un método con una nueva definición de ese mismo método.*/
+}
