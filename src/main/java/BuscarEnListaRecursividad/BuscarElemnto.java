b6f3993f1c5dd0cc207fb6ceaa6ad5e8195d16c0
Hind <hindsamiri2@gmail.com>
2022-03-18 13:43:53 +0100

    añadir nuevo ejercicio

---
+++ src/main/java/BuscarEnListaRecursividad/BuscarElemnto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BuscarEnListaRecursividad;
+
+/**
+ *
+ * @author hinda
+ */
+public class BuscarElemnto {
+    public static void main(String[] args) {
+        //buscar elemto en una lista con la recursevidad
+    }
+    
+}
